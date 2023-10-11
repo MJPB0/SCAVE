93a939c2d1f253aaa8973d2671afcd0150597d62
Mateusz Barczuk <mjpbtv@gmail.com>
2023-10-11 21:57:10 +0200

    feat: add ui placeholder to indicate when player can interact with an object

--- Assets/Scripts/Shared/Utils/Constants.cs
+++ Assets/Scripts/Shared/Utils/Constants.cs
@@ -5,5 +5,6 @@ using UnityEngine;
 public class Constants {
     public static string MINEABLE_TAG { get; } = "Mineable";
     public static string PICKABLE_TAG { get; } = "Pickable";
+    public static string INTERACTABLE_TAG { get; } = "Interactable";
     public static string ORE_PARENT_TAG { get; } = "Ore drops";
 }
